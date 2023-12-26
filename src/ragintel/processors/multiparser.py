39ee785ef71322b37ba335fc4a84124212255da0
darkquasar <darkquasar7@gmail.com>
2023-12-26 11:40:41 +1000

    refactoring repo for src folder

---
+++ src/ragintel/processors/multiparser.py
@@ -0,0 +1,25 @@
+import io
+import re
+
+import docx
+import pdfplumber
+from bs4 import BeautifulSoup
+
+
+def parse_text(file_name: str, content: io.BytesIO) -> str:
+    if file_name.endswith(".pdf"):
+        with pdfplumber.open(content) as pdf:
+            text = " ".join(page.extract_text() for page in pdf.pages)
+    elif file_name.endswith(".html"):
+        text = BeautifulSoup(
+            content.read().decode("utf-8"), features="html.parser"
+        ).get_text()
+    elif file_name.endswith(".txt"):
+        text = content.read().decode("utf-8")
+    elif file_name.endswith(".docx"):
+        text = " ".join(
+            paragraph.text for paragraph in docx.Document(content).paragraphs
+        )
+
+    cleaned_text = re.sub(r"\s+", " ", text).strip()
+    return cleaned_text
